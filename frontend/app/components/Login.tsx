3a15602cc1cb636b4120dd7fd1db3776778f7602
Mohak157 <girimohak73@gmail.com>
2026-02-15 18:43:36 +0545

    Login page

--- frontend/app/components/Login.tsx
+++ frontend/app/components/Login.tsx
@@ -1,15 +1,47 @@
 "use client";
-import React from 'react'
-import { loginaction } from './Loginfunc';
+import React,{useState,ChangeEvent} from 'react';
+import { useRouter } from 'next/navigation';
 
-  const login = ()=>{
-  return (
+
+
+  const Login = ()=>{
+     const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e:React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/auth/jwt/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        username: email,
+        password: password,
+      }),
+    });
+
+    if (!response.ok) {
+      alert("Invalid credentials");
+      return;
+    }
+
+    const data = await response.json();
+    localStorage.setItem("access_token", data.access_token);
+
+    router.push("/feed");
+  };
+
+
+    return (
     <main className='flex items-center justify-center min-h-screen'>
   
-      <form action={loginaction} className='flex flex-col gap-4'>
+      <form onSubmit={handleLogin} className='flex flex-col gap-4'>
         <h1 className='text-center'>LOGIN </h1>
-        <input type="email" name = "email" placeholder='Enter your email' required className='p-1 border' />
-        <input type="password" name = "password" placeholder='Enter your password' required className='p-1 border' />
+        <input type="email" value={email}  onChange={(e)=>setEmail(e.target.value)} name = "email" placeholder='Enter your email' required className='p-1 border' />
+        <input type="password" value={password} onChange ={(e)=>setPassword(e.target.value)} name = "password" placeholder='Enter your password' required className='p-1 border' />
         <button type='submit' className='px-1'>Login</button>
       </form>
 
@@ -17,4 +49,4 @@ import { loginaction } from './Loginfunc';
   )
 }
 
-export default login
+export default Login
--- frontend/app/components/Loginfunc.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-   export async function loginaction(formData:FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
-
-    
-   }
-    
